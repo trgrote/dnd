a18f6e761cf5994172091efdd2a44d8eb308a156
Taylor Grote <grotese@gmail.com>
2022-05-01 09:45:42 -0500

    Add initiative route page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Initiative from './routes/initiative/Initiative';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Link
+                  className="App-link"
+                  to="initiative"
+                >
+                  Go To Initiative
+                </Link>
+              </>
+            }/>
+            <Route path="initiative" element={<Initiative/>}/>
+          </Routes>
       </header>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/initiative/Initiative.js
@@ -0,0 +1,14 @@
+import logo from '../../logo.svg';
+
+const Initiative = () => {
+    return (
+    <>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Initiative!
+        </p>
+    </>
+    );
+}
+
+export default Initiative;
